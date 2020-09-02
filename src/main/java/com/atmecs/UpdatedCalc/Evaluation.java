7e35ae61521db6553ae7ed319e90ee4f01177c37
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-09-02 16:42:43 +0530

    Updated

--- src/main/java/com/atmecs/UpdatedCalc/Evaluation.java
+++ src/main/java/com/atmecs/UpdatedCalc/Evaluation.java
@@ -3,27 +3,37 @@ package com.atmecs.updatedcalc;
 
 import java.util.Scanner;
 
-public class Evaluation {
-
-	public void evaluation() {
-
+public class Evaluation 
+{
+	public void evaluation() 
+	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Input your Expression");
 		String expression1 = sc.next();
 		String[] operands = expression1.split("[\\+\\-\\*\\/]");
 		String[] operators = expression1.split("\\d+");
 		int result = 0;
-		for (int index = 0; index < operators.length; index++) {
-			int number = Integer.parseInt(operands[index]);
-			if (operators[index].equals("+")) {
+		for (int i= 0; i < operators.length; i++) 
+		{
+			int number = Integer.parseInt(operands[i]);
+			if (operators[i].equals("+")) 
+			{
 				result = result + number;
-			} else if (operators[index].equals("-")) {
+			} 
+			else if (operators[i].equals("-")) 
+			{
 				result = result - number;
-			} else if (operators[index].equals("*")) {
+			} 
+			else if (operators[i].equals("*")) 
+			{
 				result = result * number;
-			} else if (operators[index].equals("/")) {
+			} 
+			else if (operators[i].equals("/"))
+			{
 				result = result / number;
-			} else {
+			} 
+			else
+			{
 				result = result + number;
 			}
 		}
